64eec9a3682c8609e3d5ac6a9eb36a7d6cff9bfe
RyuJeongSang <42318201+RyuJeongSang@users.noreply.github.com>
2023-01-08 00:42:46 +0900

    login form

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { LoginForm } from "./login-form";
+import { LoginedPage } from "./logined-page";
+
+const LoginContainer = styled.main`
+  background-color: wheat;
+  width: 100%;
+  height: 100vh;
+`;
 
 export const Loginpage = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  // User Login info(하드코딩 just make test easy!!)
+
   return (
-    <main>
-      <h1>login!!!!</h1>
-      <form></form>
-    </main>
+    <LoginContainer>
+      {isLogin ? <LoginedPage /> : <LoginForm setIsLogin={setIsLogin} />}
+    </LoginContainer>
   );
 };
---
+++ src/components/logined-page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const LoginedPage = () => {
+  return <div>logined!!!</div>;
+};
